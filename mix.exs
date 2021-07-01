6a0e1d30bc45dbbf3e2436232d0674938092bf4a
thelastinuit <email@luisignac.io>
2021-06-30 22:24:26 -0500

    Linters and Precommit
    - Add Credo
    - Add Coveralls
    - Add Dialyzer
    - Add Precommit

--- mix.exs
+++ mix.exs
@@ -7,22 +7,61 @@ defmodule ExFacturapi.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      dialyzer: [
+        plt_add_deps: :apps_direct,
+        plt_add_apps: [
+          :ex_unit,
+          :mix
+        ]
+      ],
+      dialyzer_warnings: [
+        :error_handling,
+        :race_conditions,
+        :unknown
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp package do
+    [
+      name: "ex_facturapi",
+      maintainers: ["VentUp"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/Ventup-IT/ex_facturapi"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
+    ]
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:poison, "~> 3.1"},
+      {:httpoison, "~> 1.7"},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:dialyzex, "~> 1.2.0", only: [:dev], runtime: false},
+      {:sobelow, "~> 0.8", only: :dev}
     ]
   end
 end

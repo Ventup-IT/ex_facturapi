9fd4bfac218eea436b07f7ac6919bcf70ba026b2
thelastinuit <email@luisignac.io>
2021-06-30 20:01:36 -0500

    Init repo

---
+++ lib/ex_facturapi.ex
@@ -0,0 +1,18 @@
+defmodule ExFacturapi do
+  @moduledoc """
+  Documentation for `ExFacturapi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExFacturapi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_facturapi_test.exs
@@ -0,0 +1,8 @@
+defmodule ExFacturapiTest do
+  use ExUnit.Case
+  doctest ExFacturapi
+
+  test "greets the world" do
+    assert ExFacturapi.hello() == :world
+  end
+end
